643a25cb73606de10c4bfdde1d1fdbd3ea4d1431
Jomel Bautista <jomelgbautista@gmail.com>
2023-03-02 16:43:35 -0800

    added findLongestSubstring algo of mult sliding window pattern

---
+++ JavascriptMasteryDataStructures/6.7.findLongestSubstring.js
@@ -0,0 +1,63 @@
+// A. PATTERN: SLIDING WINDOW
+// B. PROBLEM: Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+// ---
+// STEP 1. Restate Problem, define inputs and types, define outputs and types, define labels, state assumptions, note questions, note edge cases
+// restate problem: Find length of longest substring with no repeating characters
+// inputs: string
+// outputs: int (len of longest substring)
+// labels: arr, n
+// assumptions/questions: 
+// edge cases: will need to check from front then set starting position to index of duplicate char + 1
+
+// Step 2. Explore at least 3 examples
+// findLongestSubstring('') // 0
+// findLongestSubstring('rithmschool') // 7
+// findLongestSubstring('thisisawesome') // 6
+// findLongestSubstring('thecatinthehat') // 7
+// findLongestSubstring('bbbbbb') // 1
+// findLongestSubstring('longestsubstring') // 8
+// findLongestSubstring('thisishowwedoit') // 6
+
+// Step 3. Break steps of solution
+// Step 4. Simplify and solve
+
+const findLongestSubstring = (str) => {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+// Step 4.5. Test solution
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('rithmschool')) // 7
+console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thecatinthehat')) // 7
+console.log(findLongestSubstring('bbbbbb')) // 1
+console.log(findLongestSubstring('longestsubstring')) // 8
+console.log(findLongestSubstring('thisishowwedoit')) // 6
+
+// Step 5. Look back and refactor
+// *** AVOID NESTED LOOPS ***
+
+// Step 6. Analysis
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+// Time Analysis.
+const timeMySol1 = performance.now()
+console.log(findLongestSubstring('thecatinthehat')) // 7
+const timeMySol2 = performance.now()
+console.log(`---Time Elapsed for my solution: ${(timeMySol2 - timeMySol1) / 1000} secs---`)
