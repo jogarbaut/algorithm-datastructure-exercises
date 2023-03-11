74c03053aede7b023636928b9dd3e3911f53e4fb
Jomel Bautista <jomelgbautista@gmail.com>
2023-03-10 22:08:13 -0800

    updated singly-linked-list algo

--- datastructure-exercises/singly-linked-list.js
+++ datastructure-exercises/singly-linked-list.js
@@ -30,6 +30,23 @@ class SinglyLinkedList {
     this.length += 1
     return this
   }
+  pop() {
+    if (!this.head) return undefined
+    let current = this.head
+    let newTail = current
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return current
+  }
 }
 
 let list = new SinglyLinkedList
