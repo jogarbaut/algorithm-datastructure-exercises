ecdf1427b75cde508014e08db8e3057add3363a0
Jomel Bautista <jomelgbautista@gmail.com>
2023-03-16 08:44:07 -0700

    added depth first search (inorder) of search of tree traversal

--- datastructure-exercises/binary-search-trees.js
+++ datastructure-exercises/binary-search-trees.js
@@ -141,4 +141,25 @@ class BinarySearchTree {
     // return the array of values
     return data
   }
+  // Depth first search (dfs)
+  // DFS - InOrder (recursively)
+  DFSInOrder(){
+    // create a variable to store the values of nodes visited
+    let data = []
+    // store the root of the BST in a variable called current
+    let current = this.root
+    // write a helper function which accepts a node
+    const traverse = (node) => {
+      // if node has a left property, call the helper function with the left property on the node
+      if(node.left) traverse(node.left)
+      // push the value of the node to the variable that stores the values
+      data.push(node)
+      // if node has a right property, call the helper function with the right property on the node
+      if(node.right) traverse(node.right)
+    }
+    // invoke the helper function with the current variable
+    traverse(current)
+    // return the array of values
+    return data
+  }
 }
