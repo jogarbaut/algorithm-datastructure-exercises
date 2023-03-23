6b3bd9cee2d5653134c672e6e61251b260051f13
Jomel Bautista <jomelgbautista@gmail.com>
2023-03-22 19:26:36 -0700

    added traversal methods for graphs data structure

--- datastructure-exercises/graphs.js
+++ datastructure-exercises/graphs.js
@@ -53,13 +53,110 @@ class Graph {
     // delete the key in the adjacency list for that vertex
     delete this.adjacenyList[vertex]
   }
-}
+  // Graph traversal
+    // uses
+      // peer to peer networking
+      // finding "closest" matches/recommendations
+      // shortest path problems
+      // GPS navigation
+      // solving mazes
 
-let g = new Graph()
-g.addVertex("Tokyo")
-g.addVertex("San Francisco")
-g.addVertex("San Jose")
-g.addEdge("Tokyo", "San Jose")
-g.addEdge("Tokyo", "San Francisco")
-g.removeEdge("Tokyo", "San Jose")
-console.log(g)
+    // Depth First Search (DFS)
+    // if vertex is empty, return (this is the base case)
+    // add vertext to results list
+    // mark vertex as visited
+    // for each neighbor in vertex's neighbors:
+      // if neighbor is not visited
+        // recursively call DFS on neighbor
+
+    // Depth first traversal
+  depthFirstRecursive(start){
+    // function should accept a starting node
+      // create a list to store the end result, to be returned at the very end
+      // create an object to store visited vertices
+      // create a helper function which accepts a vertex
+        // helper function should return early if the vertex is empty
+        // helper function should place vertex it accepts into the visited object and push that vertex into the result array
+        // loop over all of values in the adjacencyList for that vertex
+        // if any of those values have not been visited, recusively invoke helper function with that vertex
+      // invoke helper function with starting vertex
+      // return results array
+    const result = []
+    const visited = {}
+    const adjacencyList = this.adjacenyList
+
+    (function dfs(vertex){
+      if(!vertex) return null
+      visited[vertex] = true
+      result.push(vertex)
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          return dfs(neighbor)
+        }
+      })
+    })(start)
+    return result
+  }
+  // depth first traversal (iterative)
+  // function shoudl accept a starting node
+  // create a stack to help keep track of vertices (use a list/array)
+  // create a list to store end result, to be returned at the very end
+  // create an object to store visited vertices
+  // add starting vertex to stack and mark it visited
+  // while stack has something in it
+    // pop next vertex from the stack
+    // if vertex hasn't been visited
+      // mark it as visited
+      // add it to the result list
+      // push all of its neighbors into the stack
+    // return result array
+  depthFirstIterative(start){
+    const stack = [start]
+    const result = []
+    const visited = {}
+    visited[start] = true
+    while(stack.length){
+      currentVertex = stack.pop()
+      result.push(currentVertex)
+      this.adjacenyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          visited[neighbor] = true
+          stack.push(neighbor)
+        }
+      })
+    }
+    return result
+  }
+
+  // breadth first traversal
+  // visit neighbors at current depth first before moving to neighbors or neighbors
+
+  // function shoudl accept a starting vertex
+  // create a queue (array can be used) and place starting vertex in it
+  // create an array to store nodes visited
+  // create an object to store nodes visited
+  // mark starting vertex as visited
+  // loop as long as there is anything in queue
+  // remove first vertex from queue and push it into the array that stores nodes visited
+  // loop over each vertex in the adjacency list for the vertex visited
+  // if it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex
+  // once you have finished looping, return array of visited nodes
+  breadthFirst(start){
+    const queue = [start]
+    const result = []
+    const visited = {}
+    let currentVertex
+    visited[start] = true
+    while(queue.length){
+      currentVertex = queue.shift()
+      result.push(currentVertex)
+      this.adjacenyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          visited[neighbor] = true
+          queue.push(neighbor)
+        }
+      })
+    }
+    return result
+  }
+}
